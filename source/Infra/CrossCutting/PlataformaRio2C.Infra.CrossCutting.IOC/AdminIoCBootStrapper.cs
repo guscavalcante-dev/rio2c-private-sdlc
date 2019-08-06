c3d7e0c350a080723e28613277c9bf52e434dfb4
Rafael Ruiz <rafael.ruiz@sof.to>
2019-08-06 16:13:29 -0300

    #6572 [Scrumwise task #8822]: Alteração do mapeamentos das tabelas para os novos nomes

--- source/Infra/CrossCutting/PlataformaRio2C.Infra.CrossCutting.IOC/AdminIoCBootStrapper.cs
+++ source/Infra/CrossCutting/PlataformaRio2C.Infra.CrossCutting.IOC/AdminIoCBootStrapper.cs
@@ -15,7 +15,6 @@ using PlataformaRio2C.Application.Interfaces.Services;
 using PlataformaRio2C.Application.Services;
 using PlataformaRio2C.Domain.Interfaces;
 using PlataformaRio2C.Domain.Services;
-using PlataformaRio2C.Infra.Data.Repository.Repositories;
 using SimpleInjector;
 
 namespace PlataformaRio2C.Infra.CrossCutting.IOC
@@ -30,12 +29,9 @@ namespace PlataformaRio2C.Infra.CrossCutting.IOC
             container.Register<IPlayerService, PlayerAdminService>(Lifestyle.Scoped);
             container.Register<Application.Interfaces.Services.INegotiationConfigService, Application.Services.NegotiationConfigService>(Lifestyle.Scoped);
             container.Register<Domain.Interfaces.INegotiationConfigService, Domain.Services.NegotiationConfigService>(Lifestyle.Scoped);
-            container.Register<INegotiationConfigRepository, NegotiationConfigRepository>(Lifestyle.Scoped);
 
             container.Register<IMailAppService, MailAppService>(Lifestyle.Scoped);
             container.Register<IMailService, MailService>(Lifestyle.Scoped);
-            container.Register<IMailRepository, MailRepository>(Lifestyle.Scoped);
-            container.Register<IMailCollaboratorRepository, MailCollaboratorRepository>(Lifestyle.Scoped);
         }       
     }    
 }
