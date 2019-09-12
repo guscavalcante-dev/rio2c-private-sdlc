d55c620b596c1095bce7cc7bd64662912568076b
Rafael Ruiz <rafael.ruiz@sof.to>
2019-09-12 16:14:32 -0300

    #6699 [Scrumwise task #9008]: Alterar banco de dados para permitir cadastro de países, estados e cidades opcionais

---
+++ source/Infra/Data/PlataformaRio2C.Infra.Data.Context/SqlScripts/Deltas/20190912_160912_0005_ChangesForAddresses.sql
@@ -0,0 +1,23 @@
+﻿--must run on deploy | test: yes, not done
+--must run on deploy | prod: yes, not done
+--possible values are: "no", "yes, not done" and "yes, done"
+
+ALTER TABLE "dbo"."Addresses"
+ADD CountryId  int  NULL
+go
+
+ALTER TABLE "dbo"."Addresses"
+ADD StateId  int  NULL
+go
+
+ALTER TABLE "dbo"."Addresses"
+ALTER COLUMN CityId  int  NULL
+go
+
+ALTER TABLE "dbo"."Addresses"
+    ADD CONSTRAINT "FK_Countries_Addresses_CountryId" FOREIGN KEY ("CountryId") REFERENCES "dbo"."Countries"("Id")
+go
+
+ALTER TABLE "dbo"."Addresses"
+    ADD CONSTRAINT "FK_States_Addresses_StateId" FOREIGN KEY ("StateId") REFERENCES "dbo"."States"("Id")
+go
