689234c8b34ee8d62d7bf3f834505fa65722b6e0
Rafael Ruiz <rafael.ruiz@sof.to>
2019-09-13 09:12:36 -0300

    #6699 [Scrumwise task #9009]: Alterar modelo ER para permitir campos de países, estados e cidades opcinais no endereço

--- source/Infra/Data/PlataformaRio2C.Infra.Data.Context/SqlScripts/Deltas/20190909_074621_0003_ChangesForOnboarding.sql
+++ source/Infra/Data/PlataformaRio2C.Infra.Data.Context/SqlScripts/Deltas/20190909_074621_0003_ChangesForOnboarding.sql
@@ -1,4 +1,6 @@
-﻿--must run on deploy: yes, not done
+﻿--must run on deploy | test: yes, done
+--must run on deploy | prod: yes, not done
+
 --possible values are: "no", "yes, not done" and "yes, done"
 
 ALTER TABLE "dbo"."AttendeeOrganizations"
--- source/Infra/Data/PlataformaRio2C.Infra.Data.Context/SqlScripts/Deltas/20190911_201610_0004_ChangesForOnboardingUserTerms.sql
+++ source/Infra/Data/PlataformaRio2C.Infra.Data.Context/SqlScripts/Deltas/20190911_201610_0004_ChangesForOnboardingUserTerms.sql
@@ -1,5 +1,6 @@
 ﻿--must run on deploy | test: yes, done
 --must run on deploy | prod: yes, not done
+
 --possible values are: "no", "yes, not done" and "yes, done"
 
 ALTER TABLE "dbo"."AttendeeCollaborators"
