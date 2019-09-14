24586f34520b0fda1251e4b4510b6b904651632d
Fabio Seixas <fseixas@gmail.com>
2019-09-14 01:01:17 -0300

    #6733 [Scrumwise task #9035]: Traduzir paises

---
+++ source/Infra/Data/PlataformaRio2C.Infra.Data.Context/SqlScripts/Deltas/20190914_000515_0008_CountryTranslation.sql
@@ -0,0 +1,253 @@
+﻿--must run on deploy | test: yes, not done
+--must run on deploy | prod: yes, not done
+
+--possible values are: "no", "yes, not done" and "yes, done"
+
+update countries set name = 'Afeganistão | Afghanistan' where id =1
+update countries set name = 'Ilhas Aland | Aland Islands' where id =2
+update countries set name = 'Albânia | Albania' where id =3
+update countries set name = 'Argélia | Algeria' where id =4
+update countries set name = 'Samoa Americana | American Samoa' where id =5
+update countries set name = 'Andorra | Andorra' where id =6
+update countries set name = 'Angola | Angola' where id =7
+update countries set name = 'Anguilla | Anguilla' where id =8
+update countries set name = 'Antártica | Antarctica' where id =9
+update countries set name = 'Antígua e Barbuda | Antigua And Barbuda' where id =10
+update countries set name = 'Argentina | Argentina' where id =11
+update countries set name = 'Armênia | Armenia' where id =12
+update countries set name = 'Aruba | Aruba' where id =13
+update countries set name = 'Austrália | Australia' where id =14
+update countries set name = 'Áustria | Austria' where id =15
+update countries set name = 'Azerbaijão | Azerbaijan' where id =16
+update countries set name = 'Bahamas | Bahamas The' where id =17
+update countries set name = 'Barém | Bahrain' where id =18
+update countries set name = 'Bangladesh | Bangladesh' where id =19
+update countries set name = 'Barbados | Barbados' where id =20
+update countries set name = 'Bielorrússia | Belarus' where id =21
+update countries set name = 'Bélgica | Belgium' where id =22
+update countries set name = 'Belize | Belize' where id =23
+update countries set name = 'Benin | Benin' where id =24
+update countries set name = 'Bermudas | Bermuda' where id =25
+update countries set name = 'Butão | Bhutan' where id =26
+update countries set name = 'Bolívia | Bolivia' where id =27
+update countries set name = 'Bósnia e Herzegovina | Bosnia and Herzegovina' where id =28
+update countries set name = 'Botsuana | Botswana' where id =29
+update countries set name = 'Ilha Bouvet | Bouvet Island' where id =30
+update countries set name = 'Brasil | Brazil' where id =31
+update countries set name = 'Território Britânico do Oceano Índico | British Indian Ocean Territory' where id =32
+update countries set name = 'Brunei | Brunei' where id =33
+update countries set name = 'Bulgária | Bulgaria' where id =34
+update countries set name = 'Burkina Faso | Burkina Faso' where id =35
+update countries set name = 'Burundi | Burundi' where id =36
+update countries set name = 'Camboja | Cambodia' where id =37
+update countries set name = 'Camarões | Cameroon' where id =38
+update countries set name = 'Canadá | Canada' where id =39
+update countries set name = 'cabo Verde | Cape Verde' where id =40
+update countries set name = 'Ilhas Cayman | Cayman Islands' where id =41
+update countries set name = 'República Centro-Africana | Central African Republic' where id =42
+update countries set name = 'Chade | Chad' where id =43
+update countries set name = 'Chile | Chile' where id =44
+update countries set name = 'China | China' where id =45
+update countries set name = 'Ilha do Natal | Christmas Island' where id =46
+update countries set name = 'Ilhas Cocos (Keeling) | Cocos (Keeling) Islands' where id =47
+update countries set name = 'Colômbia | Colombia' where id =48
+update countries set name = 'Comores | Comoros' where id =49
+update countries set name = 'Congo | Congo' where id =50
+update countries set name = 'República Democrática do Congo | Congo The Democratic Republic Of The' where id =51
+update countries set name = 'Ilhas Cook | Cook Islands' where id =52
+update countries set name = 'Costa Rica | Costa Rica' where id =53
+update countries set name = 'Costa do Marfim (Costa do Marfim) | Cote D''Ivoire (Ivory Coast)' where id =54
+update countries set name = 'Croácia (Hrvatska) | Croatia (Hrvatska)' where id =55
+update countries set name = 'Cuba | Cuba' where id =56
+update countries set name = 'Chipre | Cyprus' where id =57
+update countries set name = 'República Checa | Czech Republic' where id =58
+update countries set name = 'Dinamarca | Denmark' where id =59
+update countries set name = 'Djibuti | Djibouti' where id =60
+update countries set name = 'Dominica | Dominica' where id =61
+update countries set name = 'República Dominicana | Dominican Republic' where id =62
+update countries set name = 'Timor Leste | East Timor' where id =63
+update countries set name = 'Equador | Ecuador' where id =64
+update countries set name = 'Egito | Egypt' where id =65
+update countries set name = 'El Salvador | El Salvador' where id =66
+update countries set name = 'Guiné Equatorial | Equatorial Guinea' where id =67
+update countries set name = 'Eritreia | Eritrea' where id =68
+update countries set name = 'Estônia | Estonia' where id =69
+update countries set name = 'Etiópia | Ethiopia' where id =70
+update countries set name = 'Ilhas Falkland | Falkland Islands' where id =71
+update countries set name = 'ilhas Faroe | Faroe Islands' where id =72
+update countries set name = 'Ilhas Fiji | Fiji Islands' where id =73
+update countries set name = 'Finlândia | Finland' where id =74
+update countries set name = 'França | France' where id =75
+update countries set name = 'Guiana Francesa | French Guiana' where id =76
+update countries set name = 'Polinésia Francesa | French Polynesia' where id =77
+update countries set name = 'Territórios Franceses do Sul | French Southern Territories' where id =78
+update countries set name = 'Gabão | Gabon' where id =79
+update countries set name = 'Gâmbia | Gambia The' where id =80
+update countries set name = 'Geórgia | Georgia' where id =81
+update countries set name = 'Alemanha | Germany' where id =82
+update countries set name = 'Gana | Ghana' where id =83
+update countries set name = 'Gibraltar | Gibraltar' where id =84
+update countries set name = 'Grécia | Greece' where id =85
+update countries set name = 'Gronelândia | Greenland' where id =86
+update countries set name = 'Granada | Grenada' where id =87
+update countries set name = 'Guadalupe | Guadeloupe' where id =88
+update countries set name = 'Guam | Guam' where id =89
+update countries set name = 'Guatemala | Guatemala' where id =90
+update countries set name = 'Guernsey e Alderney | Guernsey and Alderney' where id =91
+update countries set name = 'Guiné | Guinea' where id =92
+update countries set name = 'Guiné-Bissau | Guinea-Bissau' where id =93
+update countries set name = 'Guiana | Guyana' where id =94
+update countries set name = 'Haiti | Haiti' where id =95
+update countries set name = 'Ilhas Heard e McDonald | Heard and McDonald Islands' where id =96
+update countries set name = 'Honduras | Honduras' where id =97
+update countries set name = 'Hong Kong S.A.R. | Hong Kong S.A.R.' where id =98
+update countries set name = 'Hungria | Hungary' where id =99
+update countries set name = 'Islândia | Iceland' where id =100
+update countries set name = 'Índia | India' where id =101
+update countries set name = 'Indonésia | Indonesia' where id =102
+update countries set name = 'Irã | Iran' where id =103
+update countries set name = 'Iraque | Iraq' where id =104
+update countries set name = 'Irlanda | Ireland' where id =105
+update countries set name = 'Israel | Israel' where id =106
+update countries set name = 'Itália | Italy' where id =107
+update countries set name = 'Jamaica | Jamaica' where id =108
+update countries set name = 'Japão | Japan' where id =109
+update countries set name = 'Jersey | Jersey' where id =110
+update countries set name = 'Jordânia | Jordan' where id =111
+update countries set name = 'Cazaquistão | Kazakhstan' where id =112
+update countries set name = 'Quênia | Kenya' where id =113
+update countries set name = 'Kiribati | Kiribati' where id =114
+update countries set name = 'Coreia do Norte \ n | Korea North\n' where id =115
+update countries set name = 'Coreia do Sul | Korea South' where id =116
+update countries set name = 'Kuwait | Kuwait' where id =117
+update countries set name = 'Quirguistão | Kyrgyzstan' where id =118
+update countries set name = 'Laos | Laos' where id =119
+update countries set name = 'Letônia | Latvia' where id =120
+update countries set name = 'Líbano | Lebanon' where id =121
+update countries set name = 'Lesoto | Lesotho' where id =122
+update countries set name = 'Libéria | Liberia' where id =123
+update countries set name = 'Líbia | Libya' where id =124
+update countries set name = 'Liechtenstein | Liechtenstein' where id =125
+update countries set name = 'Lituânia | Lithuania' where id =126
+update countries set name = 'Luxemburgo | Luxembourg' where id =127
+update countries set name = 'Macau S.A.R. | Macau S.A.R.' where id =128
+update countries set name = 'Macedônia | Macedonia' where id =129
+update countries set name = 'Madagáscar | Madagascar' where id =130
+update countries set name = 'Malawi | Malawi' where id =131
+update countries set name = 'Malásia | Malaysia' where id =132
+update countries set name = 'Maldivas | Maldives' where id =133
+update countries set name = 'Mali | Mali' where id =134
+update countries set name = 'Malta | Malta' where id =135
+update countries set name = 'Homem (Ilha de) | Man (Isle of)' where id =136
+update countries set name = 'Ilhas Marshall | Marshall Islands' where id =137
+update countries set name = 'Martinica | Martinique' where id =138
+update countries set name = 'Mauritânia | Mauritania' where id =139
+update countries set name = 'Maurícia | Mauritius' where id =140
+update countries set name = 'Mayotte | Mayotte' where id =141
+update countries set name = 'México | Mexico' where id =142
+update countries set name = 'Micronésia | Micronesia' where id =143
+update countries set name = 'Moldova | Moldova' where id =144
+update countries set name = 'Mônaco | Monaco' where id =145
+update countries set name = 'Mongólia | Mongolia' where id =146
+update countries set name = 'Montenegro | Montenegro' where id =147
+update countries set name = 'Montserrat | Montserrat' where id =148
+update countries set name = 'Marrocos | Morocco' where id =149
+update countries set name = 'Moçambique | Mozambique' where id =150
+update countries set name = 'Myanmar | Myanmar' where id =151
+update countries set name = 'Namíbia | Namibia' where id =152
+update countries set name = 'Nauru | Nauru' where id =153
+update countries set name = 'Nepal | Nepal' where id =154
+update countries set name = 'Antilhas holandesas | Netherlands Antilles' where id =155
+update countries set name = 'Holanda | Netherlands The' where id =156
+update countries set name = 'Nova Caledônia | New Caledonia' where id =157
+update countries set name = 'Nova Zelândia | New Zealand' where id =158
+update countries set name = 'Nicarágua | Nicaragua' where id =159
+update countries set name = 'Níger | Niger' where id =160
+update countries set name = 'Nigéria | Nigeria' where id =161
+update countries set name = 'Niue | Niue' where id =162
+update countries set name = 'Ilha Norfolk | Norfolk Island' where id =163
+update countries set name = 'Ilhas Marianas do Norte | Northern Mariana Islands' where id =164
+update countries set name = 'Noruega | Norway' where id =165
+update countries set name = 'Omã | Oman' where id =166
+update countries set name = 'Paquistão | Pakistan' where id =167
+update countries set name = 'Palau | Palau' where id =168
+update countries set name = 'Território Palestino, Ocupado | Palestinian Territory Occupied' where id =169
+update countries set name = 'Panamá | Panama' where id =170
+update countries set name = 'Papua Nova Guiné | Papua new Guinea' where id =171
+update countries set name = 'Paraguai | Paraguay' where id =172
+update countries set name = 'Peru | Peru' where id =173
+update countries set name = 'Filipinas | Philippines' where id =174
+update countries set name = 'Ilha Pitcairn | Pitcairn Island' where id =175
+update countries set name = 'Polônia | Poland' where id =176
+update countries set name = 'Portugal | Portugal' where id =177
+update countries set name = 'Porto Rico | Puerto Rico' where id =178
+update countries set name = 'Catar | Qatar' where id =179
+update countries set name = 'Reunião | Reunion' where id =180
+update countries set name = 'Romênia | Romania' where id =181
+update countries set name = 'Rússia | Russia' where id =182
+update countries set name = 'Ruanda | Rwanda' where id =183
+update countries set name = 'Santa Helena | Saint Helena' where id =184
+update countries set name = 'São Cristóvão e Nevis | Saint Kitts And Nevis' where id =185
+update countries set name = 'Santa Lúcia | Saint Lucia' where id =186
+update countries set name = 'São Pedro e Miquelon | Saint Pierre and Miquelon' where id =187
+update countries set name = 'São Vicente e Granadinas | Saint Vincent And The Grenadines' where id =188
+update countries set name = 'São Bartolomeu | Saint-Barthelemy' where id =189
+update countries set name = 'Saint-Martin (parte francesa) | Saint-Martin (French part)' where id =190
+update countries set name = 'Samoa | Samoa' where id =191
+update countries set name = 'San Marino | San Marino' where id =192
+update countries set name = 'São Tomé e Príncipe | Sao Tome and Principe' where id =193
+update countries set name = 'Arábia Saudita | Saudi Arabia' where id =194
+update countries set name = 'Senegal | Senegal' where id =195
+update countries set name = 'Sérvia | Serbia' where id =196
+update countries set name = 'Seychelles | Seychelles' where id =197
+update countries set name = 'Serra Leoa | Sierra Leone' where id =198
+update countries set name = 'Cingapura | Singapore' where id =199
+update countries set name = 'Eslováquia | Slovakia' where id =200
+update countries set name = 'Eslovênia | Slovenia' where id =201
+update countries set name = 'Ilhas Salomão | Solomon Islands' where id =202
+update countries set name = 'Somália | Somalia' where id =203
+update countries set name = 'África do Sul | South Africa' where id =204
+update countries set name = 'Geórgia do Sul | South Georgia' where id =205
+update countries set name = 'Sudão do Sul | South Sudan' where id =206
+update countries set name = 'Espanha | Spain' where id =207
+update countries set name = 'Sri Lanka | Sri Lanka' where id =208
+update countries set name = 'Sudão | Sudan' where id =209
+update countries set name = 'Suriname | Suriname' where id =210
+update countries set name = 'Ilhas Svalbard e Jan Mayen | Svalbard And Jan Mayen Islands' where id =211
+update countries set name = 'Suazilândia | Swaziland' where id =212
+update countries set name = 'Suécia | Sweden' where id =213
+update countries set name = 'Suíça | Switzerland' where id =214
+update countries set name = 'Síria | Syria' where id =215
+update countries set name = 'Taiwan | Taiwan' where id =216
+update countries set name = 'Tajiquistão | Tajikistan' where id =217
+update countries set name = 'Tanzânia | Tanzania' where id =218
+update countries set name = 'Tailândia | Thailand' where id =219
+update countries set name = 'Togo | Togo' where id =220
+update countries set name = 'Tokelau | Tokelau' where id =221
+update countries set name = 'Tonga | Tonga' where id =222
+update countries set name = 'Trindade e Tobago | Trinidad And Tobago' where id =223
+update countries set name = 'Tunísia | Tunisia' where id =224
+update countries set name = 'Peru | Turkey' where id =225
+update countries set name = 'Turquemenistão | Turkmenistan' where id =226
+update countries set name = 'Ilhas Turks e Caicos | Turks And Caicos Islands' where id =227
+update countries set name = 'Tuvalu | Tuvalu' where id =228
+update countries set name = 'Uganda | Uganda' where id =229
+update countries set name = 'Ucrânia | Ukraine' where id =230
+update countries set name = 'Emirados Árabes Unidos | United Arab Emirates' where id =231
+update countries set name = 'Reino Unido | United Kingdom' where id =232
+update countries set name = 'Estados Unidos | United States' where id =233
+update countries set name = 'Ilhas Menores Distantes dos Estados Unidos | United States Minor Outlying Islands' where id =234
+update countries set name = 'Uruguai | Uruguay' where id =235
+update countries set name = 'Usbequistão | Uzbekistan' where id =236
+update countries set name = 'Vanuatu | Vanuatu' where id =237
+update countries set name = 'Estado da Cidade do Vaticano (Santa Sé) | Vatican City State (Holy See)' where id =238
+update countries set name = 'Venezuela | Venezuela' where id =239
+update countries set name = 'Vietnã | Vietnam' where id =240
+update countries set name = 'Ilhas Virgens Britânicas | Virgin Islands (British)' where id =241
+update countries set name = 'Ilhas Virgens (EUA) | Virgin Islands (US)' where id =242
+update countries set name = 'Ilhas Wallis e Futuna | Wallis And Futuna Islands' where id =243
+update countries set name = 'Saara Ocidental | Western Sahara' where id =244
+update countries set name = 'Iémen | Yemen' where id =245
+update countries set name = 'Zâmbia | Zambia' where id =246
+update countries set name = 'Zimbábue | Zimbabwe' where id =247
+						
