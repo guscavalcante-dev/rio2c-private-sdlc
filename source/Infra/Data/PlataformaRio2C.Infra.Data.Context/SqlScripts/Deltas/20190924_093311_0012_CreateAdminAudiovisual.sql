e1673919ccd24a42e5887d9fa1b7c0a724ad712f
Rafael Ruiz <rafael.ruiz@sof.to>
2019-09-24 09:35:08 -0300

    #6818 [Scrumwise task #9104]: Criar novo perfil de admin para audio visual

---
+++ source/Infra/Data/PlataformaRio2C.Infra.Data.Context/SqlScripts/Deltas/20190924_093311_0012_CreateAdminAudiovisual.sql
@@ -0,0 +1,11 @@
+﻿--must run on deploy | test: yes, done
+--must run on deploy | prod: yes, done
+
+--possible values are: "no", "yes, not done" and "yes, done"
+
+SET IDENTITY_INSERT [dbo].[Roles] ON 
+go
+INSERT [dbo].[Roles] ([Id], [Name]) VALUES (3, N'Admin | Audiovisual')
+go
+SET IDENTITY_INSERT [dbo].[Roles] OFF
+go
