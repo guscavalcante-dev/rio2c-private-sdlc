e2b58542d7a6f84eb064b14a6b2435a8a3b3b586
Rafael Ruiz <rafael.ruiz@sof.to>
2019-09-30 12:03:46 -0300

    #6832 [Scrumwise task #9169]: Alteração de banco de dados para api de players

---
+++ source/Infra/Data/PlataformaRio2C.Infra.Data.Context/SqlScripts/Deltas/20190930_114613_0001_ChangesForPlayersApi.sql
@@ -0,0 +1,28 @@
+﻿--must run on deploy | test: yes, not done
+--must run on deploy | prod: yes, not done
+
+--possible values are: "no", "yes, not done" and "yes, done"
+
+ALTER TABLE "dbo"."AttendeeOrganizations"
+ADD IsApiDisplayEnabled  bit  NULL
+go
+
+ALTER TABLE "dbo"."AttendeeCollaborators"
+ADD IsApiDisplayEnabled  bit  NULL
+go
+
+UPDATE "dbo"."AttendeeOrganizations"
+SET IsApiDisplayEnabled = 0
+go
+
+UPDATE "dbo"."AttendeeCollaborators"
+SET IsApiDisplayEnabled = 0
+go
+
+ALTER TABLE "dbo"."AttendeeOrganizations"
+ALTER COLUMN IsApiDisplayEnabled  bit  NOT NULL
+go
+
+ALTER TABLE "dbo"."AttendeeCollaborators"
+ALTER COLUMN IsApiDisplayEnabled  bit  NOT NULL
+go
