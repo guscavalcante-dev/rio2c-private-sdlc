970a4d559b4666ffc00e1398a1b2c207ce9f18bd
Rafael Ruiz <rafael.ruiz@sof.to>
2019-10-22 16:55:13 -0300

    #6990 [Scrumwise item #6990]: Integrar login/redirect com o projeto Talentos

--- source/Presentation/Web/PlataformaRio2C.Web.Site/Services/TalentPlatformService.cs
+++ source/Presentation/Web/PlataformaRio2C.Web.Site/Services/TalentPlatformService.cs
@@ -15,6 +15,7 @@ using System;
 using System.Configuration;
 using System.Net;
 using System.Net.Http;
+using System.Net.Security;
 using Newtonsoft.Json;
 using PlataformaRio2C.Domain.Dtos;
 using PlataformaRio2C.Infra.CrossCutting.Resources;
@@ -91,6 +92,7 @@ namespace PlataformaRio2C.Web.Site.Services
                 ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls |
                                                        SecurityProtocolType.Tls11 |
                                                        SecurityProtocolType.Tls12;
+                ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback (delegate { return true; });
 
                 var response = httpMethod == HttpMethod.Get ? client.DownloadString(apiUrl) :
                                                               client.UploadString(apiUrl, httpMethod.ToString(), jsonString);
