cc6eaf0db9f5756a91bd4c772fe2ac47dce92146
Rafael Ruiz <rafael.ruiz@sof.to>
2019-11-29 14:36:55 -0300

    #7118 [Scrumwise task #9434]: Enviar mensagem com todos os dados necessários (segurança)

--- source/Infra/CrossCutting/PlataformaRio2C.Infra.CrossCutting.SystemParameter/Interfaces/ISystemParameterAppService.cs
+++ source/Infra/CrossCutting/PlataformaRio2C.Infra.CrossCutting.SystemParameter/Interfaces/ISystemParameterAppService.cs
@@ -1,18 +1,18 @@
-﻿using PlataformaRio2C.Infra.CrossCutting.SystemParameter.Models;
-using PlataformaRio2C.Infra.CrossCutting.SystemParameter.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using PlataformaRio2C.Infra.CrossCutting.SystemParameter.Models;
+//using PlataformaRio2C.Infra.CrossCutting.SystemParameter.ViewModels;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace PlataformaRio2C.Infra.CrossCutting.SystemParameter
-{
-    public interface ISystemParameterAppService
-    {
-        IEnumerable<SystemParameterAppViewModel> All(bool @readonly = false);
-        void ReIndex();
-        AppValidationResult UpdateAll(IEnumerable<SystemParameterAppViewModel> systemParameterAppViewModels);
-        T Get<T>(string code);
-    }
-}
+//namespace PlataformaRio2C.Infra.CrossCutting.SystemParameter
+//{
+//    public interface ISystemParameterAppService
+//    {
+//        IEnumerable<SystemParameterAppViewModel> All(bool @readonly = false);
+//        void ReIndex();
+//        AppValidationResult UpdateAll(IEnumerable<SystemParameterAppViewModel> systemParameterAppViewModels);
+//        T Get<T>(string code);
+//    }
+//}
--- source/Presentation/Web/PlataformaRio2C.Web.Admin/Controllers/SystemParameterController.cs
+++ source/Presentation/Web/PlataformaRio2C.Web.Admin/Controllers/SystemParameterController.cs
@@ -4,7 +4,7 @@
 // Created          : 06-28-2019
 //
 // Last Modified By : Rafael Dantas Ruiz
-// Last Modified On : 08-07-2019
+// Last Modified On : 11-29-2019
 // ***********************************************************************
 // <copyright file="SystemParameterController.cs" company="Softo">
 //     Copyright (c) Softo. All rights reserved.
@@ -27,28 +27,29 @@ namespace PlataformaRio2C.Web.Admin.Controllers
     //[Authorize(Roles = "Administrator", Users = "projeto.rio2c@marlin.com.br")]
     public class SystemParameterController : BaseController
     {
-        private readonly ISystemParameterAppService _systemParameterAppService;
+        //private readonly ISystemParameterAppService _systemParameterAppService;
 
         /// <summary>Initializes a new instance of the <see cref="SystemParameterController"/> class.</summary>
         /// <param name="commandBus">The command bus.</param>
         /// <param name="identityController">The identity controller.</param>
         /// <param name="systemParameterAppService">The system parameter application service.</param>
-        public SystemParameterController(IMediator commandBus, IdentityAutenticationService identityController, ISystemParameterAppService systemParameterAppService)
+        public SystemParameterController(IMediator commandBus, IdentityAutenticationService identityController/*, ISystemParameterAppService systemParameterAppService*/)
             : base(commandBus, identityController)
         {
-            _systemParameterAppService = systemParameterAppService;
+            //_systemParameterAppService = systemParameterAppService;
         }
 
         // GET: SystemParameter
         public ActionResult Index()
         {
-            var systemParameters = _systemParameterAppService.All(true);
-            return View(systemParameters);
+            //var systemParameters = _systemParameterAppService.All(true);
+            //return View(systemParameters);
+            return null;
         }
 
         public ActionResult ReIndex()
         {
-            _systemParameterAppService.ReIndex();
+            //_systemParameterAppService.ReIndex();
             return RedirectToAction("Index");
         }
 
@@ -56,18 +57,18 @@ namespace PlataformaRio2C.Web.Admin.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Index(List<SystemParameterAppViewModel> systemParameterViewModels)
         {
-            var result = _systemParameterAppService.UpdateAll(systemParameterViewModels);
-            if (result.IsValid)
-            {
-                this.StatusMessage("Parametros atualizados com sucesso!", Infra.CrossCutting.Tools.Enums.StatusMessageType.Success);
-            }
-            else if (result.Errors != null && result.Errors.Any())
-            {
-                foreach (var error in result.Errors)
-                {
-                    this.StatusMessage(error.Message, Infra.CrossCutting.Tools.Enums.StatusMessageType.Danger);
-                }
-            }
+            //var result = _systemParameterAppService.UpdateAll(systemParameterViewModels);
+            //if (result.IsValid)
+            //{
+            //    this.StatusMessage("Parametros atualizados com sucesso!", Infra.CrossCutting.Tools.Enums.StatusMessageType.Success);
+            //}
+            //else if (result.Errors != null && result.Errors.Any())
+            //{
+            //    foreach (var error in result.Errors)
+            //    {
+            //        this.StatusMessage(error.Message, Infra.CrossCutting.Tools.Enums.StatusMessageType.Danger);
+            //    }
+            //}
 
             return RedirectToAction("Index");
         }
