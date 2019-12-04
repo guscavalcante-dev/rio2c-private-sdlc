55b919dba968480cc8c22f126f7d3bc014ffe5bc
Rafael Ruiz <rafael.ruiz@sof.to>
2019-12-03 23:19:37 -0300

    #7118 [Scrumwise task #9471]: Melhorar performance do menu de conversas

---
+++ source/Infra/Data/PlataformaRio2C.Infra.Data.Context/SqlScripts/Deltas/20191203_225413_0007_AddIndexesToMessages.sql
@@ -0,0 +1,28 @@
+﻿--must run on deploy | test: yes, done
+--must run on deploy | prod: yes, done
+
+--possible values are: "no", "yes, not done" and "yes, done"
+
+CREATE NONCLUSTERED INDEX "IDX_Messages_SenderId" ON "dbo"."Messages"
+( 
+	"SenderId"            ASC
+)
+go
+
+CREATE NONCLUSTERED INDEX "IDX_Messages_RecipientId" ON "dbo"."Messages"
+( 
+	"RecipientId"         ASC
+)
+go
+
+CREATE NONCLUSTERED INDEX "IDX_Messages_NotificationEmailSendDate" ON "dbo"."Messages"
+( 
+	"NotificationEmailSendDate"  ASC
+)
+go
+
+CREATE NONCLUSTERED INDEX "IDX_Messages_ReadDate" ON "dbo"."Messages"
+( 
+	"ReadDate"            ASC
+)
+go
