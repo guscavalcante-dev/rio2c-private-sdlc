b29b0409f1b99cbce04235e50eec82e2b6cb556b
Rafael Ruiz <rafael.ruiz@sof.to>
2019-12-18 11:01:47 -0300

    #7211 [Scrumwise task #9546]: Alterar banco de dados para permitir configuração de api e destaque por tipo de colaborador

---
+++ source/Infra/Data/PlataformaRio2C.Infra.Data.Context/SqlScripts/Deltas/20191218_102311_0010_ChangesForSpeakers.sql
@@ -0,0 +1,63 @@
+﻿--must run on deploy | test: yes, not done
+--must run on deploy | prod: yes, not done
+
+--possible values are: "no", "yes, not done" and "yes, done"
+
+ALTER TABLE "dbo"."AttendeeOrganizationTypes"
+ADD IsApiDisplayEnabled  bit  NULL
+go
+
+UPDATE "dbo"."AttendeeOrganizationTypes"
+SET IsApiDisplayEnabled = 0
+go
+
+UPDATE dbo.AttendeeOrganizationTypes
+SET IsApiDisplayEnabled = 1
+FROM
+	dbo.Organizations o
+	INNER JOIN dbo.AttendeeOrganizations ao ON o.Id = ao.OrganizationId AND ao.IsApiDisplayEnabled = 1 AND ao.EditionId = 2 AND ao.IsDeleted = 0
+	INNER JOIN dbo.AttendeeOrganizationTypes aot ON ao.Id = aot.AttendeeOrganizationId AND OrganizationTypeId = 1 AND aot.IsDeleted = 0
+WHERE
+	o.IsDeleted = 0
+go
+
+ALTER TABLE "dbo"."AttendeeOrganizationTypes"
+ALTER COLUMN IsApiDisplayEnabled  bit  NOT NULL
+go
+
+ALTER TABLE "dbo"."AttendeeOrganizationTypes"
+ADD ApiHighlightPosition  int  NULL
+go
+
+
+ALTER TABLE "AttendeeCollaboratorTypes"
+ADD IsApiDisplayEnabled  bit  NULL
+go
+
+UPDATE "dbo"."AttendeeCollaboratorTypes"
+SET IsApiDisplayEnabled = 0
+go
+
+ALTER TABLE "AttendeeCollaboratorTypes"
+ALTER COLUMN IsApiDisplayEnabled  bit  NOT NULL
+go
+
+ALTER TABLE "AttendeeCollaboratorTypes"
+ADD ApiHighlightPosition  int  NULL
+go
+
+ALTER TABLE "dbo"."AttendeeOrganizations"
+  DROP COLUMN "IsApiDisplayEnabled"
+go
+
+ALTER TABLE "dbo"."AttendeeOrganizations"
+  DROP COLUMN "ApiHighlightPosition"
+go
+
+ALTER TABLE "dbo"."AttendeeCollaborators"
+  DROP COLUMN "IsApiDisplayEnabled"
+go
+
+ALTER TABLE "dbo"."AttendeeCollaborators"
+  DROP COLUMN "ApiHighlightPosition"
+go
