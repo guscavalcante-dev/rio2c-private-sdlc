9b95b847c7221621a71b6f5383e86ae7380d9b0e
Rafael Ruiz <rafael.ruiz@sof.to>
2020-01-16 11:16:53 -0300

    #7280 [Scrumwise task #9728]: Alterar banco de dados para cadastro de palestrantes

---
+++ source/Infra/Data/PlataformaRio2C.Infra.Data.Context/SqlScripts/Deltas/20200116_110051_0015_ChangesForSpeakersSocialMedias.sql
@@ -0,0 +1,44 @@
+﻿--must run on deploy | test: yes, hot done
+--must run on deploy | prod: yes, hot done
+
+--possible values are: "no", "yes, not done" and "yes, done"
+
+ALTER TABLE "dbo"."Organizations" 
+   ALTER COLUMN "Website" varchar(300)
+go
+
+ALTER TABLE "dbo"."Organizations"
+ADD Linkedin  varchar(100)  NULL
+go
+
+ALTER TABLE "dbo"."Organizations"
+ADD Instagram  varchar(100)  NULL
+go
+
+ALTER TABLE "dbo"."Organizations"
+ADD Twitter  varchar(100)  NULL
+go
+
+ALTER TABLE "dbo"."Organizations"
+ADD Youtube  varchar(300)  NULL
+go
+
+ALTER TABLE "dbo"."Collaborators"
+ADD Website  varchar(300)  NULL
+go
+
+ALTER TABLE "dbo"."Collaborators"
+ADD Linkedin  varchar(100)  NULL
+go
+
+ALTER TABLE "dbo"."Collaborators"
+ADD Instagram  varchar(100)  NULL
+go
+
+ALTER TABLE "dbo"."Collaborators"
+ADD Twitter  varchar(100)  NULL
+go
+
+ALTER TABLE "dbo"."Collaborators"
+ADD Youtube  varchar(300)  NULL
+go
