dae4af8beb4069349f0cb906a5929e24461c1f3c
Rafael Dantas Ruiz <rafael.ruiz@sof.to>
2020-01-31 16:33:48 -0300

    #7336 [Scrumwise item #7336]: Alterar descrição da url na tab do navegador

--- source/Infra/CrossCutting/PlataformaRio2C.Infra.CrossCutting.Resources/Labels.Designer.cs
+++ source/Infra/CrossCutting/PlataformaRio2C.Infra.CrossCutting.Resources/Labels.Designer.cs
@@ -19,7 +19,7 @@ namespace PlataformaRio2C.Infra.CrossCutting.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Labels {
@@ -4552,7 +4552,7 @@ namespace PlataformaRio2C.Infra.CrossCutting.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Bem-vindo ao Rio2C | RioContentMarket {0}!.
+        ///   Looks up a localized string similar to Bem-vindo ao Rio2C {0}.
         /// </summary>
         public static string Welcome {
             get {
