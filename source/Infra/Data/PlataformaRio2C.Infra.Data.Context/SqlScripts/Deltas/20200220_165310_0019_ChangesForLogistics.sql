35d897e2df4769ae5c82b248aa89c8411519c852
Rafael Dantas Ruiz <rafael.ruiz@sof.to>
2020-02-20 16:55:42 -0300

    #7292 [Scrumwise task #9769]: Deve exitir um campo para escolher o tipo de data

---
+++ source/Infra/Data/PlataformaRio2C.Infra.Data.Context/SqlScripts/Deltas/20200220_165310_0019_ChangesForLogistics.sql
@@ -0,0 +1,28 @@
+﻿--must run on deploy | test: yes, not done
+--must run on deploy | prod: yes, not done
+
+--possible values are: "no", "yes, not done" and "yes, done"
+
+ALTER TABLE "dbo"."Logistics"
+  DROP COLUMN "AirfareOtherLogisticSponsor"
+go
+
+ALTER TABLE "dbo"."Logistics"
+  DROP COLUMN "AccommodationOtherLogisticSponsor"
+go
+
+ALTER TABLE "dbo"."Logistics"
+  DROP COLUMN "AirportTransferOtherLogisticSponsor"
+go
+
+ALTER TABLE "dbo"."Logistics"
+ADD AdditionalInfo  varchar(1000)  NULL
+go
+
+ALTER TABLE "AttendeeLogisticSponsors"
+ADD IsOther  bit  NOT NULL
+go
+
+ALTER TABLE "Places"
+ADD IsAirport  bit  NOT NULL
+go
