068389159907a44e0a7f7fa5b8d7ee8543193c18
Leonardo <fanhero@sof.to>
2021-04-30 17:00:23 -0300

    feat: Alteração credencial de acesso de administradores [RIO2CMY-51]

--- source/Presentation/Web/PlataformaRio2C.Web.Admin/Assets/js/managers/managers.changepassword.widget.js
+++ source/Presentation/Web/PlataformaRio2C.Web.Admin/Assets/js/managers/managers.changepassword.widget.js
@@ -6,8 +6,8 @@ var ManagersChangePassword = function () {
         MyRio2cCommon.block();
 
         var jsonParameters = new Object();
-        jsonParameters.userId = 2;
-        jsonParameters.newPassword = "!MinhaSenha123";
+        jsonParameters.userId = userId;
+        jsonParameters.newPassword = newPassword;
 
         $.post(MyRio2cCommon.getUrlWithCultureAndEdition('/Managers/UpdatePassword'), jsonParameters, function (data) {
             MyRio2cCommon.handleAjaxReturn({
@@ -38,11 +38,31 @@ var ManagersChangePassword = function () {
             });
     };
 
-    var showModal = function (collaboratorUid) {
-        var message = 'Confirme a alteração de senha'; // labels.changePasswordConfirmationMessage;
+    var showModal = function (userId) {
+        //var message = 'Confirme a alteração de senha'; // labels.changePasswordConfirmationMessage;
+
+        var html = '<form action="xyzzzzzz" enctype="multipart/form-data" id="ChangePasswordManagerForm" method="post">                    <div id="form-container">';
+        html += '   <div class="kt-section">';
+        html += '     <input id="IsCreatingNewManager" name="IsCreatingNewManager" type="hidden" value="True">';
+        html += '     <div class="kt-section__content mt-3">';      
+        html += '       <div class="form-group row">';
+        html += '           <div class="col-md-6" style="position: relative;">';
+        html += '               <label class="control-label" for="Password">Senha</label>';
+        html += '               <input autocomplete="off" class="form-control showhidepassword maxlength-enabled valid" data-val="true" data-val-length="O campo Senha deve ter entre 6 e 100 caracteres." data-val-length-max="100" data-val-length-min="6" data-val-requiredif="O campo é obrigatório." data-val-requiredif-dependentproperty="IsCreatingNewManager" data-val-requiredif-dependentvalue="True" data-val-requiredif-operator="EqualTo" id="Password" name="Password" type="password">';
+        html += '               <span class="text-danger field-validation-valid" data-valmsg-for="Password" data-valmsg-replace="true"></span>';
+        html += '           </div>';
+        html += '           <div class="col-md-6">';
+        html += '               <label class="control-label" for="ConfirmPassword">Confirmação de senha</label>';
+        html += '               <input autocomplete="off" class="form-control showhidepassword" data-val="true" data-val-equalto="Senha de confirmação incorreta." data-val-equalto-other="*.Password" id="ConfirmPassword" name="ConfirmPassword" type="password">';
+        html += '               <span class="field-validation-valid text-danger" data-valmsg-for="ConfirmPassword" data-valmsg-replace="true"></span>';
+        html += '           </div>';
+        html += '       </div>';
+        html += '     </div>';
+        html += '   </div>';
+        html += '</form>';
 
         bootbox.dialog({
-            message: message,
+            message: html,
             buttons: {
                 cancel: {
                     label: labels.cancel,
@@ -54,11 +74,32 @@ var ManagersChangePassword = function () {
                     label: labels.confirm,
                     className: "btn btn-danger",
                     callback: function () {
-                        executeChangePassword(collaboratorUid);
+                        var formIdOrClass = "#ChangePasswordManagerForm";
+                        var validator = $(formIdOrClass).validate({
+                            debug: true
+                        });
+
+                        if ($(formIdOrClass).valid()) {
+                            MyRio2cCommon.block();
+                            //$(formIdOrClass).submit();
+                            executeChangePassword(2, $("#Password").val());
+                        }
+                        else {
+                            validator.focusInvalid();
+                            return false;
+                        }
+
+                        // $("#ChangePasswordManagerForm").submit();
+
+                        //return false;
+
+                        
                     }
                 }
             }
         });
+
+        MyRio2cCommon.enableFormValidation({ formIdOrClass: "#ChangePasswordManagerForm", enableHiddenInputsValidation: true, enableMaxlength: true });
     };
 
     return {
