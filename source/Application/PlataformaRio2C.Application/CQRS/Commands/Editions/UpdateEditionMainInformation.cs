3dd6c6c7276ec2ceb24f8950cb794393dedda111
Rafael Dantas Ruiz <rafael.ruiz@sof.to>
2021-08-24 11:26:13 -0300

    fix: Correção da estrutura de comandos da edição [RIO2CMY-317]

--- source/Application/PlataformaRio2C.Application/CQRS/Commands/Editions/UpdateEditionMainInformation.cs
+++ source/Application/PlataformaRio2C.Application/CQRS/Commands/Editions/UpdateEditionMainInformation.cs
@@ -3,8 +3,8 @@
 // Author           : Renan Valentim
 // Created          : 08-21-2021
 //
-// Last Modified By : Renan Valentim
-// Last Modified On : 08-21-2021
+// Last Modified By : Rafael Dantas Ruiz
+// Last Modified On : 08-24-2021
 // ***********************************************************************
 // <copyright file="UpdateEditionMainInformation.cs" company="Softo">
 //     Copyright (c) Softo. All rights reserved.
@@ -16,13 +16,14 @@ using PlataformaRio2C.Domain.Dtos;
 namespace PlataformaRio2C.Application.CQRS.Commands
 {
     /// <summary>UpdateEditionMainInformation</summary>
-    public class UpdateEditionMainInformation : EditionBaseCommand
+    public class UpdateEditionMainInformation : EditionMainInformationBaseCommand
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="UpdateEditionMainInformation" /> class.
         /// </summary>
         /// <param name="editionDto">The edition event dto.</param>
-        public UpdateEditionMainInformation(EditionDto editionDto) : base(editionDto, true, false)
+        public UpdateEditionMainInformation(EditionDto editionDto) 
+            : base(editionDto)
         {
         }
 
