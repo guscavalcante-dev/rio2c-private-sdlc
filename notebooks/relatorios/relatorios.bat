d65f0c1b3b575545e7c13a3bc2d90584b5426a7a
Rafael Franco Jose dos Santos <rafael.apfsantos@outlook.com>
2022-01-13 15:45:04 -0300

    feat: Python for generate reports executivos and players by edition [RIO2CMY-437]

---
+++ notebooks/relatorios/relatorios.bat
@@ -0,0 +1,4 @@
+"python.exe" "%~dp0relatorio-players-por-edicao.py"  --edition=2022
+"python.exe" "%~dp0relatorio-executivos-por-edicao.py"  --edition=2022
+pause
+    
---
+++ notebooks/relatorios/settings.py
@@ -0,0 +1,7 @@
+DEFAULT_FILE_PATH = './files/'
+
+DESTINATARIOS = ['rafael.franco@sof.to', 'lorena.zabot@sof.to']
+REMETENTE = 'rafael.franco@sof.to'
+
+# KEYS
+SENDGRID_API_KEY = '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'
