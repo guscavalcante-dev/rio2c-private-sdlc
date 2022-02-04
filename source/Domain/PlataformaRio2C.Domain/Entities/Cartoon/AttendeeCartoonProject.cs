3ff0796af5c137cd6099ab84bc7ed913c79da300
Rafael Franco Jose dos Santos <rafael.apfsantos@outlook.com>
2022-02-04 16:24:05 -0300

    feat: create entities for attende cartoon [RIO2CMY-491]

--- source/Domain/PlataformaRio2C.Domain/Entities/Cartoon/AttendeeCartoonProject.cs
+++ source/Domain/PlataformaRio2C.Domain/Entities/Cartoon/AttendeeCartoonProject.cs
@@ -32,6 +32,10 @@ namespace PlataformaRio2C.Domain.Entities
         public virtual Edition Edition { get; private set; }
         public virtual CartoonProject CartoonProject { get; private set; }
 
+        public virtual ICollection<AttendeeCartoonProjectCollaborator> AttendeeCartoonProjectCollaborators { get; private set; }
+        public virtual ICollection<CartoonProject> CartoonProjects { get; private set; }
+        public virtual ICollection<AttendeeCartoonProjectEvaluation> AttendeeCartoonProjectEvaluations { get; private set; }
+
         /// <summary>Initializes a new instance of the <see cref="AttendeeCartoonProject"/> class.</summary>
         /// <param name="edition">The edition.</param>
         /// <param name="musicBand">The music band.</param>
@@ -53,12 +57,163 @@ namespace PlataformaRio2C.Domain.Entities
         {
         }
 
+
         /// <summary>Deletes the specified user identifier.</summary>
         /// <param name="userId">The user identifier.</param>
         public new void Delete(int userId)
         {
+            if (this.FindAllCartoonsProjectsNotDeleted()?.Any() == true)
+            {
+                return;
+            }
+
+            this.DeleteAttendeeCartonProjectdCollaborators(userId);
+            if (this.FindAllAttendeeCartoonProjectsCollaboratorsNotDeleted()?.Any() == true)
+            {
+                return;
+            }
+
             base.Delete(userId);
         }
+
+        #region Attendee Cartoon Project Collaborators
+
+        /// <summary>Deletes the attendee cartoon project collaborators.</summary>
+        /// <param name="userId">The user identifier.</param>
+        private void DeleteAttendeeCartonProjectdCollaborators(int userId)
+        {
+            foreach (var attendeCollaborator in this.FindAllAttendeeCartoonProjectsCollaboratorsNotDeleted())
+            {
+                attendeCollaborator.Delete(userId);
+            }
+        }
+
+        /// <summary>Finds all attendee cartoon project collaborators not deleted.</summary>
+        /// <returns></returns>
+        private List<AttendeeCartoonProjectCollaborator> FindAllAttendeeCartoonProjectsCollaboratorsNotDeleted()
+        {
+            return this.AttendeeCartoonProjectCollaborators?.Where(aoc => !aoc.IsDeleted)?.ToList();
+        }
+
+        
+
+        /// <summary>Gets the last created cartoon project.</summary>
+        /// <returns></returns>
+        public CartoonProject GetLastCreatedAttendeCartoonProject()
+        {
+            return this.CartoonProjects?.OrderByDescending(p => p.CreateDate).FirstOrDefault();
+        }
+
+        /// <summary>Finds all cartoons projects not deleted.</summary>
+        /// <returns></returns>
+        private List<CartoonProject> FindAllCartoonsProjectsNotDeleted()
+        {
+            return this.CartoonProjects?.Where(mp => !mp.IsDeleted)?.ToList();
+        }
+
+        #endregion
+
+        #region Evaluation
+
+        /// <summary>
+        /// Evaluates the specified evaluation user.
+        /// </summary>
+        /// <param name="evaluatorUser">The evaluation user.</param>
+        /// <param name="grade">The grade.</param>
+        public void Evaluate(User evaluatorUser, decimal grade)
+        {
+            if (this.AttendeeCartoonProjectEvaluations == null)
+                this.AttendeeCartoonProjectEvaluations = new List<AttendeeCartoonProjectEvaluation>();
+
+            var existentEvaluation = this.GetAttendeeCartoonProjectEvaluationByEvaluatorId(evaluatorUser.Id);
+            if (existentEvaluation != null)
+            {
+                existentEvaluation.Update(grade, evaluatorUser.Id);
+            }
+            else
+            {
+                this.AttendeeCartoonProjectEvaluations.Add(new AttendeeCartoonProjectEvaluation(
+                    this,
+                    evaluatorUser,
+                    grade,
+                    evaluatorUser.Id));
+            }
+
+            this.Grade = this.GetAverageEvaluation();
+            this.EvaluationsCount = this.GetAttendeeCartoonProjectEvaluationTotalCount();
+            this.LastEvaluationDate = DateTime.UtcNow;
+        }
+
+        /// <summary>
+        /// Recalculates the grade.
+        /// </summary>
+        public void RecalculateGrade()
+        {
+            this.Grade = this.GetAverageEvaluation();
+        }
+
+        /// <summary>
+        /// Recalculates the votes count.
+        /// </summary>
+        public void RecalculateVotesCount()
+        {
+            this.EvaluationsCount = this.GetAttendeeCartoonProjectEvaluationTotalCount();
+        }
+
+        /// <summary>
+        /// Gets the average evaluation.
+        /// </summary>
+        /// <returns></returns>
+        private decimal? GetAverageEvaluation()
+        {
+            if (this.FindAllAttendeeCartoonProjectEvaluationsNotDeleted()?.Any() != true)
+            {
+                return null;
+            }
+
+            // Can only generate the 'AverageEvaluation' when the 'AttendeeCartoonProjectEvaluation' count 
+            // is greater or equal than minimum necessary evaluations quantity
+            if (this.GetAttendeeCartoonProjectEvaluationTotalCount() >= this.Edition?.CartoonCommissionMinimumEvaluationsCount)
+            {
+                return this.FindAllAttendeeCartoonProjectEvaluationsNotDeleted().Sum(e => e.Grade) / this.FindAllAttendeeCartoonProjectEvaluationsNotDeleted().Count;
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Gets the attendee cartoon projects evaluation by evaluator identifier.
+        /// </summary>
+        /// <param name="evaluatorUserId">The evaluator user identifier.</param>
+        /// <returns></returns>
+        private AttendeeCartoonProjectEvaluation GetAttendeeCartoonProjectEvaluationByEvaluatorId(int evaluatorUserId)
+        {
+            return this.FindAllAttendeeCartoonProjectEvaluationsNotDeleted().FirstOrDefault(ambe =>
+                ambe.AttendeeCartoonProject.EditionId == this.EditionId &&
+                ambe.EvaluatorUserId == evaluatorUserId);
+        }
+
+        /// <summary>
+        /// Gets the attendee cartoon project  evaluation total count.
+        /// </summary>
+        /// <returns></returns>
+        private int GetAttendeeCartoonProjectEvaluationTotalCount()
+        {
+            return this.FindAllAttendeeCartoonProjectEvaluationsNotDeleted()
+                .Count(ambe => ambe.AttendeeCartoonProject.EditionId == this.EditionId);
+        }
+
+        /// <summary>
+        /// Gets the not deleted cartoon project evaluations.
+        /// OBS.: Allways use this method to calc Average and others! Evaluations calcs cannot consider deleted Evaluations!
+        /// </summary>
+        /// <returns></returns>
+        private List<AttendeeCartoonProjectEvaluation> FindAllAttendeeCartoonProjectEvaluationsNotDeleted()
+        {
+            return this.AttendeeCartoonProjectEvaluations?.Where(aoc => !aoc.IsDeleted)?.ToList();
+        }
+
+        #endregion
 
         #region Validations
 
---
+++ source/Domain/PlataformaRio2C.Domain/Entities/Cartoon/AttendeeCartoonProjectEvaluation.cs
@@ -0,0 +1,124 @@
+﻿// ***********************************************************************
+// Assembly         : PlataformaRio2C.Domain
+// Author           : Rafael Franco
+// Created          : 02-04-2022
+//
+// Last Modified By : Rafael Franco
+// Last Modified On : 02-04-2022
+// ***********************************************************************
+// <copyright file="AttendeeCartoonProjectEvaluation.cs" company="Softo">
+//     Copyright (c) Softo. All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using PlataformaRio2C.Domain.Validation;
+using PlataformaRio2C.Infra.CrossCutting.Resources;
+using System;
+
+namespace PlataformaRio2C.Domain.Entities
+{
+    /// <summary>AttendeeCartoonProjectEvaluation</summary>
+    public class AttendeeCartoonProjectEvaluation : Entity
+    {
+        public int AttendeeCartoonProjectId { get; private set; }
+        public int EvaluatorUserId { get; private set; }
+        public decimal Grade { get; private set; }
+
+        public virtual AttendeeCartoonProject AttendeeCartoonProject { get; private set; }
+        public virtual User EvaluatorUser { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AttendeeCartoonProjectEvaluation"/> class.
+        /// </summary>
+        /// <param name="attendeeCartoonProject">The attendee cartoon project.</param>
+        /// <param name="evaluatorUser">The evaluator user.</param>
+        /// <param name="userId">The user identifier.</param>
+        public AttendeeCartoonProjectEvaluation(
+            AttendeeCartoonProject attendeeCartoonProject,
+            User evaluatorUser, 
+            decimal grade,
+            int userId)
+        {
+            this.AttendeeCartoonProject = attendeeCartoonProject;
+            this.EvaluatorUser = evaluatorUser;
+            this.AttendeeCartoonProjectId = attendeeCartoonProject.Id;
+            this.EvaluatorUserId = evaluatorUser.Id;
+            this.Grade = grade;
+
+            this.IsDeleted = false;
+            this.CreateDate = this.UpdateDate = DateTime.UtcNow;
+            this.CreateUserId = this.UpdateUserId = userId;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AttendeeCartoonProjectEvaluation"/> class.
+        /// </summary>
+        protected AttendeeCartoonProjectEvaluation()
+        {
+        }
+
+        /// <summary>
+        /// Updates the specified grade.
+        /// </summary>
+        /// <param name="grade">The grade.</param>
+        /// <param name="userId">The user identifier.</param>
+        public void Update(decimal grade, int userId)
+        {
+            this.Grade = grade;
+
+            this.IsDeleted = false;
+            this.UpdateDate = DateTime.UtcNow;
+            this.UpdateUserId = userId;
+        }
+
+        /// <summary>
+        /// Deletes the specified user identifier.
+        /// </summary>
+        /// <param name="userId">The user identifier.</param>
+        public new void Delete(int userId)
+        {
+            base.Delete(userId);
+            this.AttendeeCartoonProject.RecalculateGrade();
+            this.AttendeeCartoonProject.RecalculateVotesCount();
+        }
+
+        #region Validations
+
+        /// <summary>Returns true if ... is valid.</summary>
+        /// <returns>
+        ///   <c>true</c> if this instance is valid; otherwise, <c>false</c>.</returns>
+        public override bool IsValid()
+        {
+            this.ValidationResult = new ValidationResult();
+
+            this.ValidateGrade();
+            this.ValidateEvaluatorUser();
+
+            return this.ValidationResult.IsValid;
+        }
+
+        /// <summary>
+        /// Validates the grade.
+        /// </summary>
+        public void ValidateGrade()
+        {
+            if (this.Grade < 0 || this.Grade > 10)
+            {
+                this.ValidationResult.Add(new ValidationError(string.Format(Messages.PropertyBetweenDates, Labels.Grade, "10", "0"), new string[] { "Grade" }));
+            }
+        }
+
+        /// <summary>
+        /// Validates the evaluator user.
+        /// </summary>
+        public void ValidateEvaluatorUser()
+        {
+            if (this.EvaluatorUser == null)
+            {
+                this.ValidationResult.Add(new ValidationError(string.Format(Messages.TheFieldIsRequired, Labels.EvaluatorUser), new string[] { "EvaluatorUserId" }));
+            }
+        }
+
+        #endregion
+    }
+}
