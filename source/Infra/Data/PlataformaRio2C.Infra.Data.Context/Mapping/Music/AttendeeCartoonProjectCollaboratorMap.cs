1a95479428165749ccb4828fc66bab5f5c4ce111
Rafael Franco Jose dos Santos <rafael.apfsantos@outlook.com>
2022-02-04 16:47:32 -0300

    feat: create mappers for attendee cartoons [RIO2CMY-491]

---
+++ source/Infra/Data/PlataformaRio2C.Infra.Data.Context/Mapping/Music/AttendeeCartoonProjectCollaboratorMap.cs
@@ -0,0 +1,37 @@
+﻿// ***********************************************************************
+// Assembly         : PlataformaRio2C.Infra.Data.Context
+// Author           : Rafael Franco
+// Created          : 02-04-2022
+//
+// Last Modified By : Rafael Franco
+// Last Modified On : 02-04-2022
+// ***********************************************************************
+// <copyright file="AttendeeCartoonProjectCollaboratorMap.cs" company="Softo">
+//     Copyright (c) Softo. All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using PlataformaRio2C.Domain.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace PlataformaRio2C.Infra.Data.Context.Mapping
+{
+    /// <summary>AttendeeCartoonProjectCollaboratorMap</summary>
+    public class AttendeeCartoonProjectCollaboratorMap : EntityTypeConfiguration<AttendeeCartoonProjectCollaborator>
+    {
+        /// <summary>Initializes a new instance of the <see cref="AttendeeMusicBandCollaboratorMap"/> class.</summary>
+        public AttendeeCartoonProjectCollaboratorMap()
+        {
+            this.ToTable("AttendeeCartoonProjectCollaborator");
+
+            // Relationships
+            this.HasRequired(t => t.AttendeeCartoonProject)
+                .WithMany(e => e.AttendeeCartoonProjectCollaborators)
+                .HasForeignKey(d => d.AttendeeCartoonProjectId);
+
+            this.HasRequired(t => t.AttendeeCollaborator)
+                .WithMany(e => e.AttendeeCartoonProjectCollaborators)
+                .HasForeignKey(d => d.AttendeeCollaboratorId);
+        }
+    }
+}
---
+++ source/Infra/Data/PlataformaRio2C.Infra.Data.Context/Mapping/Music/AttendeeMusicBandEvaluationMap - Copy.cs
@@ -0,0 +1,37 @@
+﻿// ***********************************************************************
+// Assembly         : PlataformaRio2C.Infra.Data.Context
+// Author           : Renan Valentim
+// Created          : 03-30-2021
+//
+// Last Modified By : Renan Valentim
+// Last Modified On : 03-30-2021
+// ***********************************************************************
+// <copyright file="AttendeeMusicBandEvaluationMap.cs" company="Softo">
+//     Copyright (c) Softo. All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using PlataformaRio2C.Domain.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace PlataformaRio2C.Infra.Data.Context.Mapping
+{
+    /// <summary>AttendeeMusicBandEvaluationMap</summary>
+    public class AttendeeMusicBandEvaluationMap : EntityTypeConfiguration<AttendeeMusicBandEvaluation>
+    {
+        /// <summary>Initializes a new instance of the <see cref="AttendeeMusicBandMap"/> class.</summary>
+        public AttendeeMusicBandEvaluationMap()
+        {
+            this.ToTable("AttendeeMusicBandEvaluations");
+
+            // Relationships
+            this.HasRequired(t => t.AttendeeMusicBand)
+                .WithMany(e => e.AttendeeMusicBandEvaluations)
+                .HasForeignKey(d => d.AttendeeMusicBandId);
+
+            this.HasRequired(t => t.EvaluatorUser)
+                .WithMany(u => u.AttendeeMusicBandEvaluations)
+                .HasForeignKey(d => d.EvaluatorUserId);
+        }
+    }
+}
