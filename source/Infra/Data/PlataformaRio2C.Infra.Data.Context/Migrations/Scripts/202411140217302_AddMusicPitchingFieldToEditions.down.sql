6088033acef277d60635e63b3b3e5f2e4f0b1c25
Gilson Oliveira <gilson.oliveira@sof.to>
2024-11-18 14:00:45 -0300

    feat: add new music pitching fields

---
+++ source/Infra/Data/PlataformaRio2C.Infra.Data.Context/Migrations/Scripts/202411140217302_AddMusicPitchingFieldToEditions.down.sql
@@ -0,0 +1,32 @@
+﻿BEGIN TRY
+	BEGIN TRANSACTION
+		ALTER TABLE dbo.Editions DROP COLUMN MusicPitchingMaximumProjectsInEdition;
+		ALTER TABLE dbo.Editions DROP COLUMN MusicPitchingMaximumProjectsPerAttendee;
+		ALTER TABLE dbo.Editions DROP COLUMN MusicPitchingMaximumApprovedProjectsPerMember;
+		ALTER TABLE dbo.Editions DROP COLUMN MusicPitchingMaximumApprovedProjectsByMembers;
+		ALTER TABLE dbo.Editions DROP COLUMN MusicPitchingEvaluationStartDateByCurator;
+		ALTER TABLE dbo.Editions DROP COLUMN MusicPitchingMaximumApprovedProjectsPerCurator;
+		ALTER TABLE dbo.Editions DROP COLUMN MusicPitchingMaximumApprovedProjectsPerPopularVote;
+		ALTER TABLE dbo.Editions DROP COLUMN MusicPitchingEvaluationStartDateByPopularVote;
+		ALTER TABLE dbo.Editions DROP COLUMN MusicPitchingEvaluationStartDateByRepechage;
+		ALTER TABLE dbo.Editions DROP COLUMN MusicPitchingMaximumApprovedProjectsPerRepechage;
+	COMMIT TRAN -- Transaction Success!
+END TRY
+BEGIN CATCH
+	IF @@TRANCOUNT > 0
+		ROLLBACK TRAN --RollBack in case of Error
+
+	-- Raise ERROR with RAISEERROR() Statement including the details of the exception
+	DECLARE @ErrorLine INT;
+	DECLARE @ErrorMessage NVARCHAR(4000);
+	DECLARE @ErrorSeverity INT;
+	DECLARE @ErrorState INT;
+
+	SELECT
+		@ErrorLine = ERROR_LINE(),
+		@ErrorMessage = ERROR_MESSAGE(),
+		@ErrorSeverity = ERROR_SEVERITY(),
+		@ErrorState = ERROR_STATE();
+		 
+	RAISERROR ('Error found in line %i: %s', @ErrorSeverity, @ErrorState, @ErrorLine, @ErrorMessage) WITH SETERROR
+END CATCH
