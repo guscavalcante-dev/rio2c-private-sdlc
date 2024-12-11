e3510ad7907d30bd6b10ee35cc5403565377ca45
Renan Valentim <rn.oliveira95@gmail.com>
2024-12-11 15:42:43 -0300

    feat: changes after code review [RIO2CMY-1380]

--- source/Infra/Data/PlataformaRio2C.Infra.Data.Context/Migrations/202412111830573_UpdateForInterestsAndInterestGroups.cs
+++ source/Infra/Data/PlataformaRio2C.Infra.Data.Context/Migrations/202412111830573_UpdateForInterestsAndInterestGroups.cs
@@ -1,16 +1,8 @@
-﻿namespace PlataformaRio2C.Infra.Data.Context.Migrations
+﻿using PlataformaRio2C.Infra.Data.Context.Helpers;
+
+namespace PlataformaRio2C.Infra.Data.Context.Migrations
 {
-    using System;
-    using System.Data.Entity.Migrations;
-    
-    public partial class UpdateForInterestsAndInterestGroups : DbMigration
+    public partial class UpdateForInterestsAndInterestGroups : SqlMigration
     {
-        public override void Up()
-        {
-        }
-        
-        public override void Down()
-        {
-        }
     }
 }
