7c406b871a261be5f5d5c543e2d986a6bec87e53
Daniel Giese Rodrigues <daniel.giese@sof.to>
2025-01-06 12:23:16 -0300

    fix:Ajuste no formulário de cadastro de novo player nas rodadas de negócio de música

---
+++ source/Infra/Data/PlataformaRio2C.Infra.Data.Context/Migrations/Scripts/202501061512587_AttMusicPlayerFormFix.down.sql
@@ -0,0 +1,30 @@
+﻿BEGIN TRY
+	BEGIN TRANSACTION
+		   -----------------------------------------------
+        -- Update IsDeleted to 1 produtor musical in TargetAudiences records
+        -----------------------------------------------
+        UPDATE [dbo].[TargetAudiences]
+        SET [IsDeleted] = 0
+        WHERE [Uid] IN (
+            'ABF2CDE5-88FF-4B8E-82F2-5E8F0F16D9A4' -- Produtor Musical
+        );
+	COMMIT TRAN -- Transaction Success!
+END TRY
+BEGIN CATCH
+	IF @@TRANCOUNT > 0
+		ROLLBACK TRAN --RollBack in case of Error
+
+	-- Raise ERROR with RAISEERROR() Statement including the details of the exception
+	DECLARE @ErrorLine INT;
+	DECLARE @ErrorMessage NVARCHAR(4000);
+	DECLARE @ErrorSeverity INT;
+	DECLARE @ErrorState INT;
+
+	SELECT
+		@ErrorLine = ERROR_LINE(),
+		@ErrorMessage = ERROR_MESSAGE(),
+		@ErrorSeverity = ERROR_SEVERITY(),
+		@ErrorState = ERROR_STATE();
+		 
+	RAISERROR ('Error found in line %i: %s', @ErrorSeverity, @ErrorState, @ErrorLine, @ErrorMessage) WITH SETERROR
+END CATCH      
