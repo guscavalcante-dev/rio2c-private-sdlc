a3afe20cf7ce16e6e7c7f41761ae5536733ff383
Daniel Giese Rodrigues <daniel.giese@sof.to>
2025-01-22 17:01:51 -0300

    Migration fix for prod Database

---
+++ source/Infra/Data/PlataformaRio2C.Infra.Data.Context/Migrations/202501222000286_UpdateMigrations22012025.cs
@@ -0,0 +1,18 @@
+﻿namespace PlataformaRio2C.Infra.Data.Context.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateMigrations22012025 : DbMigration
+    {
+        public override void Up()
+        {
+          
+        }
+        
+        public override void Down()
+        {
+         
+        }
+    }
+}
