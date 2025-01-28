01e66e8fab1f9aefc88a45a4c979a0ca9cfad125
Daniel Giese Rodrigues <daniel.giese@sof.to>
2025-01-28 18:19:06 -0300

    feat:Migration criando fake column on "MusicBusinessRoundProjectBuyerEvaluations" ("AttendeeCollaboratorId")

---
+++ source/Infra/Data/PlataformaRio2C.Infra.Data.Context/Migrations/Scripts/202501282100584_AddAttendeCollabIdFieldToMusicProject.up.sql
@@ -0,0 +1,21 @@
+﻿BEGIN TRY
+    BEGIN TRANSACTION
+
+        -- Add new column to MusicBusinessRoundProjectBuyerEvaluations table
+        ALTER TABLE "MusicBusinessRoundProjectBuyerEvaluations" 
+        ADD "AttendeeCollaboratorId" int NULL;
+
+    COMMIT TRANSACTION
+END TRY
+BEGIN CATCH
+    IF @@TRANCOUNT > 0
+        ROLLBACK TRANSACTION;
+
+    -- Re-raise the error
+    DECLARE @ErrorMessage NVARCHAR(4000), @ErrorSeverity INT, @ErrorState INT;
+    SELECT 
+        @ErrorMessage = ERROR_MESSAGE(),
+        @ErrorSeverity = ERROR_SEVERITY(),
+        @ErrorState = ERROR_STATE();
+    RAISERROR (@ErrorMessage, @ErrorSeverity, @ErrorState);
+END CATCH;
