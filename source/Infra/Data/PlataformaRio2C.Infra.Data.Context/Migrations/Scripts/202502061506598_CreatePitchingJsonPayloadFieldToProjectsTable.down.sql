c2a1c6060eda1dacb5052fc201ccca672cf0da84
Daniel Giese Rodrigues <daniel.giese@sof.to>
2025-02-06 12:18:09 -0300

    feat: RIO2CMY-1444 [Audiovisual] Implementar datas de inicio e término de inscrição de pitching (Migration Only)

---
+++ source/Infra/Data/PlataformaRio2C.Infra.Data.Context/Migrations/Scripts/202502061506598_CreatePitchingJsonPayloadFieldToProjectsTable.down.sql
@@ -0,0 +1,21 @@
+﻿BEGIN TRY
+    BEGIN TRANSACTION
+
+        -- Remove the column PitchingJsonPayload from Projects table
+        ALTER TABLE "Projects" 
+        DROP COLUMN "PitchingJsonPayload";
+
+    COMMIT TRANSACTION
+END TRY
+BEGIN CATCH
+    IF @@TRANCOUNT > 0
+        ROLLBACK TRANSACTION;
+
+    -- Re-raise the error
+    DECLARE @ErrorMessage NVARCHAR(4000), @ErrorSeverity INT, @ErrorState INT;
+    SELECT 
+        @ErrorMessage = ERROR_MESSAGE(),
+        @ErrorSeverity = ERROR_SEVERITY(),
+        @ErrorState = ERROR_STATE();
+    RAISERROR (@ErrorMessage, @ErrorSeverity, @ErrorState);
+END CATCH;
