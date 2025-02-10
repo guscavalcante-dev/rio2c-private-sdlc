f04633f91b2ebae5c346fc574ed0d2186b7a4c90
Daniel Giese Rodrigues <daniel.giese@sof.to>
2025-02-10 15:18:33 -0300

    fix:after code review.

--- source/Application/PlataformaRio2C.Application/CQRS/CommandsHandlers/Audiovisual/Collaborators/UpdateAudiovisualCollaboratorInterestsCommandHandler.cs
+++ source/Application/PlataformaRio2C.Application/CQRS/CommandsHandlers/Audiovisual/Collaborators/UpdateAudiovisualCollaboratorInterestsCommandHandler.cs
@@ -88,7 +88,7 @@ namespace PlataformaRio2C.Application.CQRS.CommandsHandlers
 
             collaborator.UpdateCollaboratorInterests(
                 edition,
-                ProjectType.AudiovisualBusinessRound,
+                ProjectType.AudiovisualPitching,
                 attendeeCollaboratorInterests,
                 cmd.UserId);
             if (!collaborator.IsValid())
