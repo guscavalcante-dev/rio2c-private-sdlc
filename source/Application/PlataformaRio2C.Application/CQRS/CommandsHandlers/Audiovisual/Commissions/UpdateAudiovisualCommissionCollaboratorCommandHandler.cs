5c445e03817fe56f78b89d0eb632e9d48c57fb4a
Daniel Giese Rodrigues <daniel.giese@sof.to>
2025-02-24 17:19:32 -0300

    hotfix:Corrigindo comportamento de herdar interesses do ultimo email cadastrado.

--- source/Application/PlataformaRio2C.Application/CQRS/CommandsHandlers/Audiovisual/Commissions/UpdateAudiovisualCommissionCollaboratorCommandHandler.cs
+++ source/Application/PlataformaRio2C.Application/CQRS/CommandsHandlers/Audiovisual/Commissions/UpdateAudiovisualCommissionCollaboratorCommandHandler.cs
@@ -102,7 +102,7 @@ namespace PlataformaRio2C.Application.CQRS.CommandsHandlers
                 await this.editionRepo.GetAsync(cmd.EditionUid ?? Guid.Empty),
                 await this.collaboratorTypeRepo.FindByNameAsync(cmd.CollaboratorTypeName),
                 cmd.IsAddingToCurrentEdition,
-                ProjectType.AudiovisualBusinessRound,
+                ProjectType.AudiovisualPitching,
                 cmd.FirstName,
                 cmd.LastNames,
                 cmd.Email,
