dffd48324a0ae034487e41e437f80048e26f21bf
Daniel Giese Rodrigues <daniel.giese@sof.to>
2025-03-11 13:32:46 -0300

    fix:correcoes pos merge

---
+++ source/Application/PlataformaRio2C.Application/CQRS/Commands/Music/MusicBusinessRoundNegotiations/CreateAttendeeMusicBusinessRoundNegotiationCollaborator.cs
@@ -0,0 +1,35 @@
+﻿// ***********************************************************************
+// Assembly         : PlataformaRio2C.Application
+// Author           : Rafael Ribeiro 
+// Created          : 05-03-2025
+//
+// Last Modified By : Rafael Ribeiro 
+// Last Modified On : 05-03-2025
+// ***********************************************************************
+// <copyright file="CreateAttendeeNegotiationCollaborator.cs" company="Softo">
+//     Copyright (c) Softo. All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
+using System;
+
+namespace PlataformaRio2C.Application.CQRS.Commands
+{
+    /// <summary>CreateAttendeeNegotiationCollaborator</summary>
+    public class CreateAttendeeMusicBusinessRoundNegotiationCollaborator : MusicbusinessRoundnegotiationBaseCommand
+    {
+        internal Guid MusicBusinesRoundNegotiationUid;
+
+        public Guid MusicBusinessRoundNegotiationUid { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CreateAttendeeMusicBusinessNegotiationCollaborator"/> class.
+        /// </summary>
+        /// <param name="negotiationUid">The negotiation uid.</param>
+        public CreateAttendeeMusicBusinessRoundNegotiationCollaborator(Guid negotiationUid)
+        {
+            this.MusicBusinesRoundNegotiationUid = negotiationUid;
+        }
+    }
+}
