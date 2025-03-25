641c95c7ab6db49589e2935569b958214845a4c5
Daniel Giese Rodrigues <daniel.giese@sof.to>
2025-03-25 13:05:58 -0300

    feat:RIO2CMY-1538 - Novas Exigencias do cliente

--- source/Domain/PlataformaRio2C.Domain/Entities/Collaborator/Collaborator.cs
+++ source/Domain/PlataformaRio2C.Domain/Entities/Collaborator/Collaborator.cs
@@ -4039,8 +4039,12 @@ namespace PlataformaRio2C.Domain.Entities
                 "SpeakerTermsAcceptanceDate",
                 new
                 {
-                    IsValid = this.AttendeeCollaborators?.Where(ct => ct.SpeakerTermsAcceptanceDate != null && ct.EditionId == editionId)?.Any(),
+                    IsValid = true,
                     Message = Messages.ImageAuthorizationForm
+
+                    //1538 - This item was requested by the client to be removed from this edition, but i will leave commented to the future. It may be useful.
+                    //IsValid = this.AttendeeCollaborators?.Where(ct => ct.SpeakerTermsAcceptanceDate != null && ct.EditionId == editionId)?.Any(),
+                    //Message = Messages.ImageAuthorizationForm
                 }
             );
         }
