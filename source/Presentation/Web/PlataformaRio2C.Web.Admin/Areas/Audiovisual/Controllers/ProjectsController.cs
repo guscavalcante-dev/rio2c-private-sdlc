a9910e023d466db289b7859f4a401bc1f3f7125c
Daniel Giese Rodrigues <daniel.giese@sof.to>
2025-04-15 12:24:26 -0300

    testfix:Ajustes PR

--- source/Presentation/Web/PlataformaRio2C.Web.Admin/Areas/Audiovisual/Controllers/ProjectsController.cs
+++ source/Presentation/Web/PlataformaRio2C.Web.Admin/Areas/Audiovisual/Controllers/ProjectsController.cs
@@ -3,8 +3,8 @@
 // Author           : Rafael Dantas Ruiz
 // Created          : 06-28-2019
 //
-// Last Modified By : Gilson Oliveira
-// Last Modified On : 10-30-2024
+// Last Modified By : Daniel Giese Rodrigues
+// Last Modified On : 04-15-2025
 // ***********************************************************************
 // <copyright file="ProjectsController.cs" company="Softo">
 //     Copyright (c) Softo. All rights reserved.
